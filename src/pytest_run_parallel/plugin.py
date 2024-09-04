ff6075864c8c5f2e766dd71693e868cff19217ff
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2024-09-04 11:15:04 -0500

    feat: add ThreadComparator class fixture to compare values across threads

--- src/pytest_run_parallel/plugin.py
+++ src/pytest_run_parallel/plugin.py
@@ -1,8 +1,15 @@
 import pytest
 import threading
 import functools
+import types
 
 from _pytest.outcomes import Skipped, Failed
+
+try:
+    import numpy as np
+    numpy_available = True
+except ImportError:
+    numpy_available = False
 
 
 def pytest_addoption(parser):
@@ -77,3 +84,62 @@ def pytest_itemcollected(item):
         n_workers = int(m.args[0])
     if n_workers is not None and n_workers > 1:
         item.obj = wrap_function_parallel(item.obj, n_workers)
+
+
+@pytest.fixture
+def num_parallel_threads(request):
+    node = request.node
+    n_workers = request.config.option.parallel_threads
+    m = node.get_closest_marker('parallel_threads')
+    if m is not None:
+        n_workers = int(m.args[0])
+    return n_workers
+
+
+class ThreadComparator:
+
+    def __init__(self, n_threads):
+        self._barrier = threading.Barrier(n_threads)
+        self._reset_evt = threading.Event()
+        self._thread_ids = []
+        self._values = {}
+
+    def compare(self, values):
+        tid = threading.get_ident()
+        self._values[tid] = values
+        self._thread_ids.append(tid)
+        self._barrier.wait()
+
+        if tid == self._thread_ids[0]:
+            thread_ids = list(self._values)
+            for value_name in values:
+                for i in range(1, len(thread_ids)):
+                    tid_a = thread_ids[i - 1]
+                    tid_b = thread_ids[i]
+                    value_a = self._values[tid_a][value_name]
+                    value_b = self._values[tid_b][value_name]
+                    try:
+                        assert type(value_a) is type(value_b)
+                        if numpy_available and isinstance(value_a, np.ndarray):
+                            if len(value_a.shape) == 0:
+                                assert value_a == value_b
+                            else:
+                                assert np.allclose(value_a, value_b)
+                        elif isinstance(value_a, types.FunctionType):
+                            assert id(value_a) == id(value_b)
+                        else:
+                            assert value_a == value_b
+                    finally:
+                        self._reset_evt.set()
+
+            self._thread_ids = []
+            self._values = {}
+            self._barrier.reset()
+            self._reset_evt.set()
+        else:
+            self._reset_evt.wait()
+
+
+@pytest.fixture
+def thread_comp(num_parallel_threads):
+    return ThreadComparator(num_parallel_threads)

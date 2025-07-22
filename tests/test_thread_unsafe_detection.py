2cd7c9c6fe811115e2eaac81b934cf8ae4c86ea0
Liam DeVoe <orionldevoe@gmail.com>
2025-07-22 18:37:45 -0400

    guard against hypothesis not being installed

--- tests/test_thread_unsafe_detection.py
+++ tests/test_thread_unsafe_detection.py
@@ -2,6 +2,13 @@ import os
 import sys
 import textwrap
 
+import pytest
+
+try:
+    import hypothesis
+except ImportError:
+    hypothesis = None
+
 
 WARNINGS_IS_THREADSAFE = bool(
     getattr(sys.flags, "context_aware_warnings", 0)
@@ -665,6 +672,7 @@ Hello world'''
     )
 
 
+@pytest.mark.skipif(hypothesis is None, reason="hypothesis needs to be installed")
 def test_thread_unsafe_hypothesis_config_option(pytester):
     pytester.makepyfile("""
     from hypothesis import given, strategies as st, settings, HealthCheck

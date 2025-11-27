632d3ef3787c3d0f39cc0511f2d3f5a2507a89cf
Liam DeVoe <orionldevoe@gmail.com>
2025-11-27 04:07:31 -0500

    use tryfirst instead

--- src/pytest_run_parallel/plugin.py
+++ src/pytest_run_parallel/plugin.py
@@ -238,7 +238,15 @@ class RunParallelPlugin:
 
         return True
 
-    @pytest.hookimpl(trylast=True)
+    # This is tryfirst=True because we need our plugin's pytest_collection_finish
+    # to be called before the terminal plugin's pytest_collection_finish:
+    #
+    # - pytest's terminal plugin hooks pytest_collection_finish, which calls
+    #   pytest_report_collectionfinish
+    # - we hook pytest_report_collectionfinish, assuming _handle_collected_item
+    #   has already been called for all items when pytest_report_collectionfinish
+    #   is called
+    @pytest.hookimpl(tryfirst=True)
     def pytest_collection_finish(self, session):
         for item in session.items:
             self._handle_collected_item(item)

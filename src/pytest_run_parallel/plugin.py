d5bbf6160d792fdbfad1d60a9c9f316d498eee9a
Liam DeVoe <orionldevoe@gmail.com>
2025-11-27 03:46:33 -0500

    only run for selected session items

--- src/pytest_run_parallel/plugin.py
+++ src/pytest_run_parallel/plugin.py
@@ -239,7 +239,11 @@ class RunParallelPlugin:
         return True
 
     @pytest.hookimpl(trylast=True)
-    def pytest_itemcollected(self, item):
+    def pytest_collection_finish(self, session):
+        for item in session.items:
+            self._handle_collected_item(item)
+
+    def _handle_collected_item(self, item):
         if not hasattr(item, "obj"):
             if not hasattr(item, "_parallel_custom_item"):
                 warnings.warn(
